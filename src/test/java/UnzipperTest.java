df63fe5a84257c8d31351a88fe314f749346c89f
Zaj <126434386+zajcomm@users.noreply.github.com>
2024-08-03 22:41:16 -0400

    Update UnzipperTest.java

--- src/test/java/UnzipperTest.java
+++ src/test/java/UnzipperTest.java
@@ -1,14 +1,17 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.io.File;
 
 public class UnzipperTest {
 
     @Test
-    public void testUnzipFunction() {
-        Unzipper unzipper = new Unzipper();
-        // Example method call to test
-        boolean result = unzipper.unzip("path/to/zipfile");
-        assertTrue(result);
+    public void testMain() throws Exception {
+        // Call the main method of Unzipper
+        String[] args = {};
+        Unzipper.main(args);
+
+        // Check if the malicious file is created
+        File f = new File("/tmp/evil.txt");
+        assertFalse("Malicious file should not exist", f.exists());
     }
 }
-
