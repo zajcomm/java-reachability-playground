f71df4f55cc7a959b5f785953d1bb0337f112ad4
Zaj <126434386+zajcomm@users.noreply.github.com>
2024-08-03 22:47:47 -0400

    Update ButlerTest.java

--- src/test/java/ButlerTest.java
+++ src/test/java/ButlerTest.java
@@ -14,6 +14,6 @@ public class ButlerTest {
 
         butler.welcome();
 
-        assertEquals("Expected output", "Hello World", outContent.toString().trim());
+        assertEquals("Hello World", outContent.toString().trim());
     }
 }
