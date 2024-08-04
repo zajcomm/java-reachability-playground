872dfe4564dbf3175b191cc6517fed521d9b37d4
Zaj <126434386+zajcomm@users.noreply.github.com>
2024-08-03 22:48:03 -0400

    Update UnzipperTest.java

--- src/test/java/UnzipperTest.java
+++ src/test/java/UnzipperTest.java
@@ -6,11 +6,9 @@ public class UnzipperTest {
 
     @Test
     public void testMain() throws Exception {
-        // Call the main method of Unzipper
         String[] args = {};
         Unzipper.main(args);
 
-        // Check if the malicious file is created
         File f = new File("/tmp/evil.txt");
         assertFalse("Malicious file should not exist", f.exists());
     }
