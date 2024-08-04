7aeb06977f964d9e30c6aef7de18a06f318b420a
Zaj <126434386+zajcomm@users.noreply.github.com>
2024-08-03 22:26:41 -0400

    Update ButlerTest.java

--- src/test/java/ButlerTest.java
+++ src/test/java/ButlerTest.java
@@ -1,1 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 
+public class ButlerTest {
+
+    @Test
+    public void testButlerFunction() {
+        Butler butler = new Butler();
+        // Example method call to test
+        String result = butler.someMethod();
+        assertEquals("Expected Result", result);
+    }
+}
