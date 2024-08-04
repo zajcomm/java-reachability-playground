59697a0669bf7157d2bb70d6d0c71c82cd2aa153
Zaj <126434386+zajcomm@users.noreply.github.com>
2024-08-03 22:27:01 -0400

    Update UnzipperTest.java

--- src/test/java/UnzipperTest.java
+++ src/test/java/UnzipperTest.java
@@ -1,1 +1,14 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class UnzipperTest {
+
+    @Test
+    public void testUnzipFunction() {
+        Unzipper unzipper = new Unzipper();
+        // Example method call to test
+        boolean result = unzipper.unzip("path/to/zipfile");
+        assertTrue(result);
+    }
+}
 
